b56384f3777f55d7ab05a0beced974eb00bf2831
Matheus Silveira <matheus@exemplo.com>
2025-01-09 12:55:41 -0300

    ajustes finais

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="mt-4 text-center">
-        <h1>Operações Matemáticas</h1>
-        <p>Digite os Numeros</p>
+        <h1>Calculadora</h1>
+        <p>digite os números e escolha a operação aritmética:</p>
     </header>
 </template>
